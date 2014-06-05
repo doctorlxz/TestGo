038b5db4931092cb1b7d30827a1d8ee469cf298b
Linxz <linxz@chinanetcenter.com>
2014-06-05 16:14:37 +0800

    add java test

---
+++ src/AntTest.java
@@ -0,0 +1,11 @@
+package com.neusoft.test;
+/**
+*This is just a test class.
+*/
+public class AntTest{
+      public static void main(String[] args){
+           for(int i=0;i<args.length;i++){
+                 System.out.println(args[i]);
+           }
+      }
+}
